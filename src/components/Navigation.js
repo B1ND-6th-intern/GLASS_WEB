1e2e6e1a6d01d6bebd9b5b4f17e8815bbebb4c57
ldh3907 <ldh165163@gmail.com>
2021-07-14 17:57:20 +0900

    update: Navigation
    update: Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,17 +5,27 @@ import PostImg from "../img/Post.png";
 import MenuImg from "../img/Menu.png";
 import SearchImg from "../img/Search.png";
 
-const Navigation = () => {
-  const useClick = (isClcik) => {
-    const [click, setClick] = useState(isClcik);
-    return { changeSearch: setClick, click };
-  };
+const useClick = (isClcik) => {
+  const [click, setClick] = useState(isClcik);
+  return { changeSearch: setClick, click };
+};
 
+const useClick2 = (isClick2) => {
+  const [click2, setClick2] = useState(isClick2);
+  return { changeSearch2: setClick2, click2 };
+};
+
+const Navigation = () => {
   const { changeSearch, click } = useClick(0, 1);
+  const { changeSearch2, click2 } = useClick2(0, 1);
 
   const searchOnClick = (isClcik) => {
     changeSearch(!isClcik);
     console.log(isClcik);
+  };
+
+  const postOnClick = (isClick2) => {
+    changeSearch2(!isClick2);
   };
 
   return (
@@ -47,7 +57,15 @@ const Navigation = () => {
               title="검색"
             ></img>
           </button>
-          <button className="navigation-item-post">
+          {click2 === true ? (
+            <div className="navigation-item-post-form"></div>
+          ) : (
+            <div></div>
+          )}
+          <button
+            className="navigation-item-post"
+            onClick={() => postOnClick(click2)}
+          >
             <img className="navigation-item-post-img" src={PostImg}></img>
           </button>
           <button className="navigation-item-menu">
