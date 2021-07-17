11a4cc602348159226abd11b28e2c1d536c837a7
ldh3907 <ldh165163@gmail.com>
2021-07-18 01:46:51 +0900

    add : LoginBar
    add : LoginBar

---
+++ src/components/Login/LoginBar.js
@@ -0,0 +1,13 @@
+import "./LoginBar.css";
+
+const LoginBar = () => {
+  return (
+    <section id="Loginbar">
+      <div className="loginbar-container">
+        <div className="loginbar-title">로그인</div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginBar;
--- src/page/Login/LoginPage.js
+++ src/page/Login/LoginPage.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import "./LoginPage.css";
-import LoginPageBackGround1 from "../../assets/img/LoginPageBackGround1.svg";
-import LoginPageBackGround2 from "../../assets/img/LoginPageBackGround2.svg";
-import BlackLogo from "../../assets/img/BlackLogo.png";
+import LoginBar from "../../components/Login/LoginBar";
 
 const LoginPage = () => {
   return (
     <section id="Login">
-      <div className="Login-container"></div>
+      <div className="Login-container">
+        <LoginBar />
+      </div>
     </section>
   );
 };
