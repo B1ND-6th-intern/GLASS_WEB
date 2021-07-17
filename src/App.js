b85426e67753b48956dff370668bbc4905bcff1d
ldh3907 <ldh165163@gmail.com>
2021-07-17 22:19:32 +0900

    add : LoginPage add:LoginPageBackGround1, LoginPageBackGround2 imgs
    add : LoginPage add:LoginPageBackGround1, LoginPageBackGround2 imgs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { HashRouter, Route } from "react-router-dom";
 import Navigation from "./page/Nav/Navigation";
 import Contentcontainer from "./page/Main/MainPage";
+import LoginPage from "./page/Login/LoginPage";
 
 function App() {
   return (
     <HashRouter>
-      <Navigation path="/" />
-      <Contentcontainer path="/" />
+      <Route path="/" exact={true} component={Navigation} />
+      <Route path="/" exact={true} component={Contentcontainer} />
+      <Route path="/signin" component={LoginPage} />
     </HashRouter>
   );
 }
