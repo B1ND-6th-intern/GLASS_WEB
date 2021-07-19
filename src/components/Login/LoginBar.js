d4341f6281f5416f3b94198faca1af12c3cc1279
ldh3907 <ldh165163@gmail.com>
2021-07-19 18:39:28 +0900

    update : LoginBar
    hook을 사용하여 로그인 창에 뭐가 쳐졌는지 알 수 있는 기능 구현

--- src/components/Login/LoginBar.js
+++ src/components/Login/LoginBar.js
@@ -4,18 +4,29 @@ import Logo1 from "../../assets/img/LoginPageBackGround1.svg";
 import Logo2 from "../../assets/img/LoginPageBackGround2.svg";
 import Logo3 from "../../assets/img/LoginPageBackGround3.svg";
 
-const UseInputId = (initial) => {
-  const [id, setId] = useState(initial);
+const UseInputId = (initialId) => {
+  const [id, setId] = useState(initialId);
   const onChange = (event) => {
     let id = event.target.value;
-    console.log(id);
+    console.log("ID : " + id);
     setId(id);
   };
   return { id, onChange };
 };
 
+const UseInputPw = (initialPw) => {
+  const [pw, setPw] = useState(initialPw);
+  const onChange = (event) => {
+    let pw = event.target.value;
+    console.log("PW : " + pw);
+    setPw(pw);
+  };
+  return { pw, onChange };
+};
+
 const LoginBar = () => {
   const inputId = UseInputId(" ");
+  const inputPw = UseInputPw(" ");
   return (
     <div className="loginbar-container">
       <div className="loginbar-title">
@@ -24,7 +35,11 @@ const LoginBar = () => {
       </div>
       <input className="loginbar-id-input" placeholder="아이디" {...inputId} />
       <img src={Logo2} className="loginbar-img2" />
-      <input className="loginbar-pw-input" placeholder="비밀번호" />
+      <input
+        className="loginbar-pw-input"
+        placeholder="비밀번호"
+        {...inputPw}
+      />
       <img src={Logo3} className="loginbar-img3" />
       <button className="loginbar-confirm-btn">로그인</button>
     </div>
