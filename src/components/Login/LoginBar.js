ffd2c07f18b54e690b856fefd4cd71aa257d5271
ldh3907 <ldh165163@gmail.com>
2021-07-19 18:23:20 +0900

    update : LoginBar
    hook을 사용하여 로그인 창에 뭐가 쳐졌는지 알 수 있는 기능 구현

--- src/components/Login/LoginBar.js
+++ src/components/Login/LoginBar.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import "./LoginBar.css";
 import Logo1 from "../../assets/img/LoginPageBackGround1.svg";
 import Logo2 from "../../assets/img/LoginPageBackGround2.svg";
 import Logo3 from "../../assets/img/LoginPageBackGround3.svg";
 
+const UseInputId = (initial) => {
+  const [id, setId] = useState(initial);
+  const onChange = (event) => {
+    let id = event.target.value;
+    console.log(id);
+    setId(id);
+  };
+  return { id, onChange };
+};
+
 const LoginBar = () => {
+  const inputId = UseInputId(" ");
   return (
     <div className="loginbar-container">
       <div className="loginbar-title">
         로그인
         <img src={Logo1} className="loginbar-img1" />
       </div>
-      <input className="loginbar-id-input" placeholder="아이디"></input>
+      <input className="loginbar-id-input" placeholder="아이디" {...inputId} />
       <img src={Logo2} className="loginbar-img2" />
-      <input className="loginbar-pw-input" placeholder="비밀번호"></input>
+      <input className="loginbar-pw-input" placeholder="비밀번호" />
       <img src={Logo3} className="loginbar-img3" />
       <button className="loginbar-confirm-btn">로그인</button>
     </div>
