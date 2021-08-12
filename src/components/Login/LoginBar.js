e6912938a4a8da8cd92f19eebf6a0588e79840bd
ldh3907 <ldh165163@gmail.com>
2021-08-12 20:08:17 +0900

    update : LoginBar
    css 최적화, 불필요한 이미지 제거

--- src/components/Login/LoginBar.js
+++ src/components/Login/LoginBar.js
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./LoginBar.css";
-import Logo1 from "../../assets/img/LoginPageBackGround1.svg";
-import Logo2 from "../../assets/img/LoginPageBackGround2.svg";
-import Logo3 from "../../assets/img/LoginPageBackGround3.svg";
 
 const LoginBar = () => {
   const [id, setId] = useState("");
@@ -29,10 +26,7 @@ const LoginBar = () => {
 
   return (
     <form onSubmit={onSubmit} className="loginbar-container">
-      <div className="loginbar-title">
-        로그인
-        <img src={Logo1} className="loginbar-img1" />
-      </div>
+      <div className="loginbar-title">로그인</div>
       <input
         name="id"
         className="loginbar-id-input"
@@ -40,7 +34,6 @@ const LoginBar = () => {
         onChange={onChange}
         value={id}
       />
-      <img src={Logo2} className="loginbar-img2" />
       <input
         name="pw"
         className="loginbar-pw-input"
@@ -51,7 +44,7 @@ const LoginBar = () => {
       <Link to="/signup">
         <h5 id="loginbar-signup-alert">계정이 없으신가요?</h5>
       </Link>
-      <img src={Logo3} className="loginbar-img3" />
+
       <input type="submit" className="loginbar-confirm-btn" value="로그인" />
     </form>
   );
