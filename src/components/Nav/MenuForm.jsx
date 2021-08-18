1f91831b197fe323b9b80979395094fbf88c6c0c
ldh3907 <ldh165163@gmail.com>
2021-08-18 09:45:17 +0900

    add : ServiceCenter, ServiceCenter img update : Route, MenuFormr
    ServiceCenter 폼 추가

--- src/components/Nav/MenuForm.jsx
+++ src/components/Nav/MenuForm.jsx
@@ -1,14 +1,18 @@
 import "./MenuForm.css";
 import DarkMode from "../Nav/MenuItem/DarkMode";
+import SerViceCenter from "./MenuItem/ServiceCenter";
 
 const MenuForm = ({ menuIsClcik }) => {
-  const MenuItems = [{ item: <DarkMode />, name: "다크모드" }];
+  const MenuItems = [
+    { item: <DarkMode />, name: "다크모드" },
+    { item: <SerViceCenter />, name: "고객센터" },
+  ];
   return (
     <>
       {menuIsClcik ? (
         <div className="MenuForm-container">
-          {MenuItems.map((item) => (
-            <div className="menuItem-item-wrap">
+          {MenuItems.map((item, index) => (
+            <div key={index} className="menuItem-item-wrap">
               <div className="menuItem-item">{item.item}</div>
               <h5 className="menuItem-name">{item.name}</h5>
             </div>
