6b318b3733a296474742ea7bbf3dac50a54cbad8
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-08-24 19:38:10 +0900

    update : SignpupBar, App, Router
    SignupBar 부분 인증 로직 변경

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import AppRouter from "./Router";
 
 function App() {
-  // const isUser = false;
+  const isUser = false;
 
-  const isUser = {
-    name: "dh",
-  }; // <= 예시
+  // const isUser = {
+  //   name: "dh",
+  // }; // <= 예시
 
   return <AppRouter isLoggedIn={Boolean(isUser)} />;
 }
