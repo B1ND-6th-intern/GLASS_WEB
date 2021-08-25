510b8df76f03581e6acf4d41c2b206e31a247514
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-08-25 20:46:20 +0900

    update: CertificationForm
    CertificationForm 부분 재전송 카운트 로직 추가

--- src/components/Certification/CertificationForm.jsx
+++ src/components/Certification/CertificationForm.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./CertificationForm.css";
 
 const CertificationForm = () => {
@@ -8,8 +9,12 @@ const CertificationForm = () => {
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
   const [alert, setAlert] = useState("");
+  const [isPossibleReSend, setIsPossibleSend] = useState(true);
   const time = useRef(299);
   const timerId = useRef(null);
+  const reSendId = useRef();
+  const reSendCount = useRef(5);
+  const history = useHistory();
 
   const onChange = (event) => {
     const {
@@ -34,23 +39,50 @@ const CertificationForm = () => {
   }, [time.current]);
 
   useEffect(() => {
-    if (time.current < -1) {
-      window.alert(
-        "인증시간을 초과했습니다. '재전송'을 눌러 다시 인증해주시기 바랍니다."
-      );
+    if (time.current <= 0) {
+      isPossibleReSend &&
+        window.alert(
+          "인증시간을 초과했습니다. '재전송'을 눌러 다시 인증해주시기 바랍니다."
+        );
       clearInterval(timerId.current);
       setAlert("시간을 초과했습니다. 다시 인증해주세요.");
     }
   }, [second]);
 
+  const reSendCounter = (event) => {
+    if (reSendId.current.contains(event.target)) {
+      reSendCount.current -= 1;
+      console.log(reSendId);
+      if (reSendCount.current === 0) {
+        setIsPossibleSend(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", reSendCounter);
+    return () => {
+      window.addEventListener("click", reSendCounter);
+    };
+  }, []);
+
   const reSend = () => {
-    time.current = 299;
-    setAlert("인증번호가 발송 되었습니다. 가입하신 메일을 확인해주세요");
+    if (isPossibleReSend) {
+      time.current = 299;
+      setAlert(
+        `인증번호가 발송 되었습니다. 가입하신 메일을 확인해주세요 (재전송 기회 : ${reSendCount.current}회)`
+      );
+    } else {
+      reSendId.current.disabled = true;
+      window.alert("재전송 기회 5번을 초과하셨습니다. 내일 다시 시도해주세요.");
+      setAlert("재전송 기회 5번을 초과하셨습니다. 내일 다시 시도해주세요.");
+    }
   };
 
   const onSubmit = (event) => {
     event.preventDefault();
     console.log("i submit");
+    history.push("/");
   };
 
   return (
@@ -64,7 +96,12 @@ const CertificationForm = () => {
         required
       />
       <div id="certification-timer">{`${minute} : ${second}`}</div>
-      <button id="certificationForm-reSendBtn" type="button" onClick={reSend}>
+      <button
+        id="certificationForm-reSendBtn"
+        type="button"
+        ref={reSendId}
+        onClick={reSend}
+      >
         재전송
       </button>
       <p id="certificationForm-input-alert">{alert}</p>
