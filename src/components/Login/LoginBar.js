9fe0914164ef61a0da57b1c819353530fc94733f
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-08-28 20:39:19 +0900

    update : LoginBar, LoginPage
    LoginBar 부분 디자인 변경

--- src/components/Login/LoginBar.js
+++ src/components/Login/LoginBar.js
@@ -36,38 +36,53 @@ const LoginBar = () => {
   const togglePwType = () => setPwType((prev) => !prev);
 
   return (
-    <form onSubmit={onSubmit} className="loginbar-container">
-      <div className="loginbar-title">로그인</div>
-      <input
-        name="id"
-        className="loginbar-id-input"
-        placeholder="아이디"
-        onChange={onChange}
-        value={id}
-      />
+    <div id="loginBar-container">
+      <div id="loginBar-asideContainer"></div>
+      <form onSubmit={onSubmit} className="loginbar-loginContainer">
+        <div id="loginBar-inputWrap">
+          <div className="loginbar-title">로그인</div>
+          <input
+            name="id"
+            className="loginbar-id-input"
+            placeholder="아이디"
+            onChange={onChange}
+            value={id}
+          />
 
-      <input
-        name="pw"
-        // ref={element}
-        type={pwType ? "text" : "password"}
-        className="loginbar-pw-input"
-        placeholder="비밀번호"
-        onChange={onChange}
-        value={pw}
-      />
-      <button id="loginbar-pwtype-btn" type="button" onClick={togglePwType}>
-        <img
-          id="loginbar-pwtype-btn-img"
-          src={pwType ? SeePwType : ClosePwType}
-        />
-      </button>
+          <div id="loginBar-pwWrap">
+            <input
+              name="pw"
+              // ref={element}
+              type={pwType ? "text" : "password"}
+              className="loginbar-pw-input"
+              placeholder="비밀번호"
+              onChange={onChange}
+              value={pw}
+            />
+            <button
+              id="loginbar-pwtype-btn"
+              type="button"
+              onClick={togglePwType}
+            >
+              <img
+                id="loginbar-pwtype-btn-img"
+                src={pwType ? SeePwType : ClosePwType}
+              />
+            </button>
+          </div>
 
-      <Link to="/signup">
-        <h5 id="loginbar-signup-alert">계정이 없으신가요?</h5>
-      </Link>
+          <Link to="/signup">
+            <h5 id="loginbar-signup-alert">계정이 없으신가요?</h5>
+          </Link>
 
-      <input type="submit" className="loginbar-confirm-btn" value="로그인" />
-    </form>
+          <input
+            type="submit"
+            className="loginbar-confirm-btn"
+            value="로그인"
+          />
+        </div>
+      </form>
+    </div>
   );
 };
 
